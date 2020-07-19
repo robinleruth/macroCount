13e6c901d53530dc1579a4c8c5e55939111c001a
robinleruth <robin.leruth@gmail.com>
2020-07-19 03:25:09 -0400

    add cache manifest to make it available offline on smart phone

--- app/interface/web/routes.py
+++ app/interface/web/routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
-from flask import render_template
+from flask import render_template, make_response
 
 
 bp = Blueprint('main', __name__)
@@ -8,3 +8,9 @@ bp = Blueprint('main', __name__)
 @bp.route('/')
 def index():
     return render_template('index.html')
+
+@bp.route('/manifest')
+def manifest():
+    res = make_response(render_template('manifest.appcache'), 200)
+    res.headers["Content-Type"] = "text/cache-manifest"
+    return res
--- run.sh
+++ run.sh
@@ -3,6 +3,5 @@
 source venv/bin/activate
 
 export APP_ENV=prd
-export FLASK_DEBUG=1
 
 python application.py
